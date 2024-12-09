64e47117b050e38cb9c5a5ec2c29ca989d4bcdda
Dmitriy Rassolenko <52157280+Dmitriy-Rassol@users.noreply.github.com>
2024-12-09 13:58:33 +0300

    Update main.ts

--- movie_frontend/src/main.ts
+++ movie_frontend/src/main.ts
@@ -1,14 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
-import './style.css'; // Подключаем стили Tailwind CSS
+import './style.css'; 
 
-// Создание приложения Vue
 const app = createApp(App);
-
-// Создание и подключение Pinia
 const pinia = createPinia();
-app.use(pinia);
 
-// Монтируем приложение
+app.use(pinia);
 app.mount('#app');
