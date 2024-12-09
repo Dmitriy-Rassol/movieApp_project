ad6145d05471523b51a5de3a86435101b7adf57f
Dmitriy Rassolenko <52157280+Dmitriy-Rassol@users.noreply.github.com>
2024-12-09 13:52:42 +0300

    Update settings.py

--- movie_backend/backend/backend/settings.py
+++ movie_backend/backend/backend/settings.py
@@ -53,12 +53,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:5174',
-    'http://127.0.0.1:5174',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'backend.urls'
 
